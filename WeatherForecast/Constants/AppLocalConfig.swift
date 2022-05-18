c3007e528b6b965f3b70540a8e0e9bbb9cfbd957
Prabakaran M <prabakaran@divum.in>
2022-05-18 09:24:25 +0530

    Added Constants fine and Alamofire,SwiftLint,lottie-ios,Toast-Swift packages are added

---
+++ WeatherForecast/Constants/AppLocalConfig.swift
@@ -0,0 +1,12 @@
+//
+//  AppLocalConfig.swift
+//  WeatherForecast
+//
+//  Created by Prabakaran on 18/05/22.
+//
+
+import Foundation
+
+struct APPLocalConfig {
+    static let APIKey: String = "65d00499677e59496ca2f318eb68c049"
+}
---
+++ WeatherForecast/Constants/Segue.swift
@@ -0,0 +1,13 @@
+//
+//  Segue.swift
+//  WeatherForecast
+//
+//  Created by Prabakaran on 18/05/22.
+//
+
+import Foundation
+
+enum Segue: String {
+    case searchResultSegue = "SearchResultSegue"
+    case weatherDetailSegue = "WeatherDetailSegue"
+}
